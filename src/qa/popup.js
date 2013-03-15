04051ba21a33b49282c1d443d10664e4d5265af2
rsalave <r@rsal.co>
2013-03-15 15:53:44 -0400

    Finished moving all files to js-lib

--- src/qa/popup.js
+++
@@ -1,9 +0,0 @@
-function openpopup(popurl){
-    window.winpops = window.open(popurl,"","width=500,height=800,toolbar,location,status,scrollbars,menubar,resizable");
-}
-
-function write(str){
-    window.winpops.document.write(str);
-}
-
-openpopup("");
